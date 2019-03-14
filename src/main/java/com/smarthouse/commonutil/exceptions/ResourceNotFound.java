807f3d6c1e02b00483265a84a7f49224a5fea46c
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-03-14 18:54:39 +0200

    Modify messages methods
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/smarthouse/commonutil/exceptions/ResourceNotFound.java
+++ src/main/java/com/smarthouse/commonutil/exceptions/ResourceNotFound.java
@@ -8,7 +8,11 @@ public class ResourceNotFound extends RuntimeException {
     public ResourceNotFound(final String message) {
         super(message);
     }
-    public static String getNoResourceMessage(final String resourceName, final int id) {
-        return String.format("Resource %s with id = %d not found", resourceName, id);
+    public static String getNoResourceMessage(final long id) {
+        return getNoResourceMessage("Resource", id);
+    }
+
+    public static String getNoResourceMessage(final String resourceName, final long id) {
+        return String.format("%s with id %d doesn't exists", resourceName, id);
     }
 }

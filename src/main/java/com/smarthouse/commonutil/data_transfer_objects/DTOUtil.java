c1ececfe2b63295857a9ea18a2bab087ecc6d067
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-04-11 18:20:45 +0300

    Add getDeviceFromDto method
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/smarthouse/commonutil/data_transfer_objects/DTOUtil.java
+++ src/main/java/com/smarthouse/commonutil/data_transfer_objects/DTOUtil.java
@@ -1,5 +1,6 @@
 package com.smarthouse.commonutil.data_transfer_objects;
 
+import com.smarthouse.commonutil.entities.Device;
 import com.smarthouse.commonutil.entities.User;
 
 public class DTOUtil {
@@ -10,6 +11,12 @@ public class DTOUtil {
         result.setEmail(userDTO.getEmail());
         result.setPassword(userDTO.getPassword());
         result.setPhoneNumber(userDTO.getPhoneNumber());
+        return result;
+    }
+
+    public static Device getDeviceFromDto(DeviceDTO deviceDTO) {
+        Device result = new Device();
+        result.setName(deviceDTO.getName());
         return result;
     }
 
